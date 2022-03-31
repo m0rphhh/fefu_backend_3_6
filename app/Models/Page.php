a0201ce0a1c4eeacc2da4813dc88144827d16f4e
gottabeugly <johny-3-tears2014@yandex.ru>
2022-03-31 17:48:56 +1000

    Lesson2: added pages

---
+++ app/Models/Page.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $fillable = ['title', 'slug', 'text'];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
